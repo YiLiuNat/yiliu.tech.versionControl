0e2752b5e01a881f044dd2087c0e2167eb9c825c
Yi Liu - Nat <1009066669@qq.com>
2021-02-24 19:30:47 +0800

    2021-02-19

---
+++ src/lib/rem (2).js
@@ -0,0 +1,23 @@
+// var pcClient = undefined;
+
+(function (doc, win,) {
+        var docEl = doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+                var clientWidth = docEl.clientWidth;
+                if (!clientWidth) return;
+                if(clientWidth>=750){
+                    docEl.style.fontSize = '100px';
+                    // pcClient = true;
+                    // console.log(pcClient);
+                }else{
+                    docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+                    // pcClient = false;
+                    // console.log(pcClient);
+                }
+            };
+
+        if (!doc.addEventListener) return;
+        win.addEventListener(resizeEvt, recalc, false);
+        doc.addEventListener('DOMContentLoaded', recalc, false);
+    })(document, window);
